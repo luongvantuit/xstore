e3dd7797dfa0d714a5f868e142d58d30985c3633
lvtus <tuluong@mashallah.app>
2023-12-22 09:12:22 +0700

    header vn -> en

--- src/XStore/Entrypoints/Mvc/Common/Header.php
+++ src/XStore/Entrypoints/Mvc/Common/Header.php
@@ -2,25 +2,25 @@
     <div class="container-fluid">
         <div class="inner-header">
             <div class="logo">
-                <img src="./assets/img/logo.png" alt="">
+                <img src="/assets/img/logo.png" alt="">
             </div>
             <div class="header-right">
-                <img src="./assets/img/icons/search.png" alt="" class="search-trigger">
-                <img src="public/assets/img/icons/man.png" alt="">
-                <a href="#">
-                    <img src="./assets/img/icons/bag.png" alt="">
-                    <span>2</span>
+                <img src="/assets/img/icons/search.png" alt="" class="search-trigger">
+                <img src="/assets/img/icons/man.png" alt="">
+                <a href="/shops">
+                    <img src="/assets/img/icons/bag.png" alt="">
+                    <!-- <span>2</span> -->
                 </a>
             </div>
             <div class="user-access">
-                <a href="/register">Đăng ký</a>
-                <a href="/login" class="in">Đăng nhập</a>
+                <a href="/register">Sign Up</a>
+                <a href="/login" class="in">Sign In</a>
             </div>
             <nav class="main-menu mobile-menu">
                 <ul>
-                    <li><a href="/">Trang chủ</a></li>
-                    <li><a href="/products">Sản phẩm </a></li>
-                    <li><a href="/shops">Giỏ hàng</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="/shops">Orders</a></li>
                 </ul>
             </nav>
         </div>
